e894bf3b2573f3cbb52742ceb0b57dc32f9264bd
Adam K Dean <adamkdean@googlemail.com>
2013-07-25 16:46:50 +0100

    Added RSA test form

--- SecureChat/Program.cs
+++ SecureChat/Program.cs
@@ -16,7 +16,7 @@ namespace SecureChat
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMain());
+            Application.Run(new frmTest());
         }
     }
 }
---
+++ SecureChat/frmTest.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace SecureChat
+{
+    partial class frmTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtEncrypt = new System.Windows.Forms.TextBox();
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.txtDecrypt = new System.Windows.Forms.TextBox();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lblPrivate = new System.Windows.Forms.Label();
+            this.lblPublic = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtEncrypt
+            // 
+            this.txtEncrypt.Enabled = false;
+            this.txtEncrypt.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEncrypt.Location = new System.Drawing.Point(12, 201);
+            this.txtEncrypt.Multiline = true;
+            this.txtEncrypt.Name = "txtEncrypt";
+            this.txtEncrypt.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtEncrypt.Size = new System.Drawing.Size(264, 204);
+            this.txtEncrypt.TabIndex = 0;
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.Enabled = false;
+            this.btnEncrypt.Location = new System.Drawing.Point(201, 411);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnEncrypt.TabIndex = 1;
+            this.btnEncrypt.Text = "Encrypt";
+            this.btnEncrypt.UseVisualStyleBackColor = true;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // txtDecrypt
+            // 
+            this.txtDecrypt.Enabled = false;
+            this.txtDecrypt.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDecrypt.Location = new System.Drawing.Point(282, 201);
+            this.txtDecrypt.Multiline = true;
+            this.txtDecrypt.Name = "txtDecrypt";
+            this.txtDecrypt.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtDecrypt.Size = new System.Drawing.Size(264, 204);
+            this.txtDecrypt.TabIndex = 2;
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.Enabled = false;
+            this.btnDecrypt.Location = new System.Drawing.Point(471, 411);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(75, 23);
+            this.btnDecrypt.TabIndex = 3;
+            this.btnDecrypt.Text = "Decrypt";
+            this.btnDecrypt.UseVisualStyleBackColor = true;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(443, 172);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(103, 23);
+            this.btnGenerate.TabIndex = 4;
+            this.btnGenerate.Text = "Generate Keys";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // lblPrivate
+            // 
+            this.lblPrivate.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrivate.Location = new System.Drawing.Point(12, 9);
+            this.lblPrivate.Name = "lblPrivate";
+            this.lblPrivate.Size = new System.Drawing.Size(264, 159);
+            this.lblPrivate.TabIndex = 5;
+            this.lblPrivate.Text = "[Private]";
+            // 
+            // lblPublic
+            // 
+            this.lblPublic.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPublic.Location = new System.Drawing.Point(279, 9);
+            this.lblPublic.Name = "lblPublic";
+            this.lblPublic.Size = new System.Drawing.Size(264, 159);
+            this.lblPublic.TabIndex = 6;
+            this.lblPublic.Text = "[Public]";
+            // 
+            // frmTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(559, 445);
+            this.Controls.Add(this.lblPublic);
+            this.Controls.Add(this.lblPrivate);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.txtDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Controls.Add(this.txtEncrypt);
+            this.Name = "frmTest";
+            this.Text = "SecureChat";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEncrypt;
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.TextBox txtDecrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lblPrivate;
+        private System.Windows.Forms.Label lblPublic;
+    }
+}
+
