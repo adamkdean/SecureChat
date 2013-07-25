f992f0326f5621aed728c245f1fbb4ae0bd99483
Adam K Dean <adamkdean@googlemail.com>
2013-07-25 16:56:29 +0100

    Added NUnit tests, ready for testing

---
+++ SecureChat/RSAHelper.Tests/NUnitTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace RSAHelper.Tests
+{
+    [TestFixture]
+    public class NUnitTests
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            bool isWorking = true;
+            
+            Assert.IsTrue(isWorking);
+        }
+    }
+}
